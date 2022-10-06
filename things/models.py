b8a998843997beb6498e4282b6ed90bcb33c7808
Junhuai Hou <junhuai.hou@kcl.ac.uk>
2022-10-06 15:48:50 +0100

    finish

--- things/models.py
+++ things/models.py
@@ -1,9 +1,11 @@
+from enum import unique
 from unicodedata import name
 from django.db import models
 from django.db.models import Model
+from django.core.validators import MaxValueValidator, MinValueValidator 
 
 # Create your models here.
 class Thing(Model):
-    name = models.TextField()
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.TextField(unique = True, max_length=30, blank = False)
+    description = models.TextField(max_length=120)
+    quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
